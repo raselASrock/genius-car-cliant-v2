2d0d65a177e20d45a2cc86ad36fa220d83d53e8b
raSel <mdraselm413@gmail.com>
2022-11-07 21:46:14 +0600

    table

---
+++ src/Pages/Orders/Orders.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../../contexts/Authprovider/AuthProvider';
+
+const Orders = () => {
+    const {user} = useContext(AuthContext);
+    const [orders, setOrders] = useState({});
+    useEffect( () =>{
+        fetch(`http://localhost:5000/orders?email=${user.email}`)
+        .then(res => res.json())
+        .then(data => setOrders(data))
+    }, [user?.email])
+    return (
+        <div>
+            <h2 className="text-5xl">You have {orders.length} orders</h2>
+            <div className="overflow-x-auto w-full">
+  <table className="table w-full">
+
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">Hart Hagerty</div>
+              <div className="text-sm opacity-50">United States</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          Zemlak, Daniel and Leannon
+          <br/>
+          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+        </td>
+        <td>Purple</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+    </tbody>
+    
+    <tfoot>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </tfoot>
+    
+  </table>
+</div>
+        </div>
+    );
+};
+
+export default Orders;
--- src/Router/Routes/Router.js
+++ src/Router/Routes/Router.js
@@ -3,6 +3,7 @@ import Main from "../../layout/Main";
 import Checkout from "../../Pages/CheckOut/Checkout";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import Orders from "../../Pages/Orders/Orders";
 import SignUp from "../../Pages/SignUp/SignUp";
 
 
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
           path: '/checkout/:id',
           element: <Checkout></Checkout>,
           loader: ({params})=> fetch(`http://localhost:5000/services/${params.id}`)
+        },
+        {
+          path: '/orders',
+          element: <Orders></Orders>
         }
       ]
 
