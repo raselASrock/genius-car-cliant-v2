3bb8bc1c0fa5e5847f785532ab2f905871652d7c
raSel <mdraselm413@gmail.com>
2022-11-07 22:04:40 +0600

    checkbox

---
+++ src/Pages/Orders/OrderRow.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const OrderRow = ({order}) => {
+    const {serviceName,customer,phone, price, email} = order;
+    return (
+        <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <td>
+          <div className="flex items-center space-x-3">
+            <div className="avatar">
+              <div className="mask mask-squircle w-12 h-12">
+                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
+              </div>
+            </div>
+            <div>
+              <div className="font-bold">{customer}</div>
+              <div className="text-sm opacity-50">{phone}</div>
+            </div>
+          </div>
+        </td>
+        <td>
+          {serviceName}
+          <br/>
+          <span className="badge badge-ghost badge-sm">${price}</span>
+        </td>
+        <td>Purple</td>
+        <th>
+          <button className="btn btn-ghost btn-xs">details</button>
+        </th>
+      </tr>
+    );
+};
+
+export default OrderRow;
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 import { useContext } from 'react';
 import { AuthContext } from '../../contexts/Authprovider/AuthProvider';
+import OrderRow from './OrderRow';
 
 const Orders = () => {
     const {user} = useContext(AuthContext);
@@ -32,48 +33,13 @@ const Orders = () => {
       </tr>
     </thead>
     <tbody>
-
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center space-x-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
-            </div>
-          </div>
-        </td>
-        <td>
-          Zemlak, Daniel and Leannon
-          <br/>
-          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-        </td>
-        <td>Purple</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
-    </tbody>
-    
-    <tfoot>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Job</th>
-        <th>Favorite Color</th>
-        <th></th>
-      </tr>
-    </tfoot>
-    
+        {
+            orders.map(order => <OrderRow
+            key={order._id}
+            order = {order}
+            ></OrderRow>)
+        }
+    </tbody> 
   </table>
 </div>
         </div>
