1941b310a440837ddab20e52755cba2db7f791e7
raSel <mdraselm413@gmail.com>
2022-11-07 18:22:05 +0600

    order

--- src/Pages/CheckOut/Checkout.js
+++ src/Pages/CheckOut/Checkout.js
@@ -4,21 +4,41 @@ import { useLoaderData } from 'react-router-dom';
 import {AuthContext} from '../../contexts/Authprovider/AuthProvider'
 
 const Checkout = () => {
-    const {title, price} = useLoaderData();
+    const {_id, title, price} = useLoaderData();
     const {user} = useContext(AuthContext);
+
+    const handlePlaceOrder = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = `${form.firstName.value} ${form.lastName.value}`
+        const email = user?.email || 'unregistered';
+        const phone = form.phone.value;
+        const message = form.message.value;
+        const order = {
+            service: _id,
+            serviceName: title,
+            price,
+            customer: name,
+            email,
+            phone,
+            message
+        }
+
+    }
 
     return (
         <div>
             <h2 className='text-5xl'>You are about to order: {title}</h2>
             <h3 className="text-3xl">Total Price: $ {price}</h3>
-            <form>
+            <form onSubmit={handlePlaceOrder}>
             <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
-            <input type="text" placeholder="Fast Name" className="input input-bordered w-full" />
-            <input type="text" placeholder="Last Name" className="input input-bordered w-full" />
-            <input type="text" placeholder="Your Number" className="input input-bordered w-full" />
-            <input type="text" placeholder="Your Email" defaultValue={user?.email} className="input input-bordered w-full" readOnly />
+            <input name='firstName' type="text" placeholder="Fast Name" className="input input-bordered w-full" />
+            <input name='lastName' type="text" placeholder="Last Name" className="input input-bordered w-full" />
+            <input name='phone' type="text" placeholder="Your Number" className="input input-bordered w-full" />
+            <input name='email' type="text" placeholder="Your Email" defaultValue={user?.email} className="input input-bordered w-full" readOnly />
             </div>
-            <textarea className="textarea textarea-bordered h-24 w-full" placeholder="Your Message"></textarea>
+            <textarea name='message' className="textarea textarea-bordered h-24 w-full" placeholder="Your Message"></textarea>
+            <input className='btn btn-error' type="submit" value="Place Your Order" />
             </form>
         </div>
     );
