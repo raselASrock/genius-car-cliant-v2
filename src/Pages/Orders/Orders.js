225dd96d92a77ec32acd1d040f602cfb1cb27720
raSel <mdraselm413@gmail.com>
2022-11-14 22:58:57 +0600

    handle add

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -6,7 +6,7 @@ import { AuthContext } from '../../contexts/Authprovider/AuthProvider';
 import OrderRow from './OrderRow';
 
 const Orders = () => {
-    const {user} = useContext(AuthContext);
+    const {user, logOut} = useContext(AuthContext);
     const [orders, setOrders] = useState([]);
     useEffect( () =>{
         fetch(`http://localhost:5000/orders?email=${user?.email}`, {
@@ -14,8 +14,16 @@ const Orders = () => {
                 authorization: `Bearer ${localStorage.getItem('genius-token')}`
             }
         })
-        .then(res => res.json())
-        .then(data => setOrders(data))
+        .then(res => {
+            if(res.status === 401 || res.status === 403){
+                logOut()
+            }
+            return res.json()
+        })
+        .then(data => {
+            setOrders(data)
+            // console.log( "received", data);
+        })
     }, [user?.email])
 
     const handleDelete = id =>{
